d503dc59784605a2721c1ee38f1524c75f0bbc92
sidthe <r.arcea@gmail.com>
2015-12-22 20:57:56 +0100

    additional methods for trello

--- trello_module.py
+++ trello_module.py
@@ -5,31 +5,46 @@ retrieval of data from the configured Trello account
 import argparse
 import ConfigParser
 from trello import TrelloApi
+import os.path
+import json
+
+def buildTrelloToken():
+    if os.path.exists('config.cfg') == False:
+        print "Error: Create a Trello config first with -tc"
+        exit(1)
+
+    config = ConfigParser.SafeConfigParser()
+    config.read('config.cfg')
+    if config.has_section('TRELLO CONFIGURATION'):
+        if config.has_option('TRELLO CONFIGURATION', 'API_KEY') and config.has_option('TRELLO CONFIGURATION', 'USER_HASH') :
+            trelloapikey = config.get('TRELLO CONFIGURATION', 'API_KEY')
+            trellouserhash = config.get('TRELLO CONFIGURATION', 'USER_HASH')
+            trello = TrelloApi(trelloapikey, token=trellouserhash)
+            return trello
+        else:
+            print "Not all Trello access values present in config.cfg. Check your configuration file.\n"
+            exit(1)
+
+
 
 def createCards(number, boardlistid, cardpattern):
+    trello = buildTrelloToken()
     for i in xrange(number):
         cardname = cardpattern + str(i)
         trello.lists.new_card(boardlistid, name=cardname)
         print 'added card', cardname
-    print 'your list has following objects', (trello.lists.get_card(boardlistid, fields='id'))
+
+
+def displayList(boardlistid):
+    trello = buildTrelloToken()
+    listcardids = trello.lists.get_card(boardlistid, fields=['id', 'name'])
+    print '\nYour list contains the following cards'
+    for i in listcardids:
+        print json.dumps(i)
 
 
 
-#reads the config.cfg file
-config = ConfigParser.SafeConfigParser()
-config.read('config.cfg')
-if config.has_section('TRELLO CONFIGURATION'):
-    if config.has_option('TRELLO CONFIGURATION', 'API_KEY') and config.has_option('TRELLO CONFIGURATION', 'USER_HASH') :
-        try:
-            trelloapikey = config.get('TRELLO CONFIGURATION', 'API_KEY')
-            trellouserhash = config.get('TRELLO CONFIGURATION', 'USER_HASH')
 
-        except ValueError:
-            print "Not all Trello access values present in config.cfg"
-
-
-trello = TrelloApi(trelloapikey, token=trellouserhash)
-print trello
 
 
 if __name__ == '__main__':
--- visually_menu.py
+++ visually_menu.py
@@ -12,9 +12,14 @@ def visuallyMenu():
 
     visuallydescriptiontext = str('#'*70 + '\nVisually is a statistics app for Trello, Github, Bugzilla\n' + '#'*70)
     parser = argparse.ArgumentParser(description=visuallydescriptiontext)
-    parser.add_argument('-mc', action="store_true", dest='make_config_switch', default=False, help='Create Visually base configuration file')
-    parser.add_argument('-trello', action="store_true", dest='trello_config_switch', default=False, help='Add Trello access parameters')
-    parser.add_argument('-trello-cc', action="store_true", dest='trello_create_card_switch', default=False, help='Manually create new trello cards')
+
+    configfilegroup = parser.add_argument_group('Configuration File', 'This argumens allow you to create and update the configuration file')
+    configfilegroup.add_argument('-mc', action="store_true", dest='make_config_switch', default=False, help='Create default configuration file')
+    configfilegroup.add_argument('-tc', action="store_true", dest='trello_config_switch', default=False, help='Add Trello access parameters to configuration file')
+
+    trellogroup = parser.add_argument_group('Trello Control', 'This argumens allow you to manually operate Trello')
+    trellogroup.add_argument('-t-cc', action="store_true", dest='trello_create_card_switch', default=False, help='Manually create new trello cards')
+    trellogroup.add_argument('-t-dl', action="store_true", dest='trello_display_list_switch', default=False, help='Manually display cards on a list')
 
     results = parser.parse_args()
 
@@ -34,7 +39,11 @@ def visuallyMenu():
             listid = raw_input('Provide Trello listid: ')
             nrofcard = int(raw_input('Number of cards to create: '))
             namepattern = raw_input('Provide name pattern (ex:mycard): ')
-            trello_module.createCards(nrofcard,listid, namepattern)
+            trello_module.createCards(nrofcard, listid, namepattern)
+
+    if results.trello_display_list_switch==True:
+            listid = raw_input('Provide Trello listid: ')
+            trello_module.displayList(listid)
 
 if __name__ == '__main__':
     pass
